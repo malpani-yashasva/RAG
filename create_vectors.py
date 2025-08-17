301daf12f47fe95870be26e48a9d6608d29a683f
yashasva <malpaniyashasva99999@gmail.com>
2025-08-17 21:38:42 +0530

    made changes to pinecone and changed the model

--- create_vectors.py
+++ create_vectors.py
@@ -1,15 +1,22 @@
-from dotenv import load_dotenv
 import os
 import requests
+from dotenv import load_dotenv
 
-
+API_URL = "https://router.huggingface.co/hf-inference/models/BAAI/bge-small-en-v1.5/pipeline/feature-extraction"
 load_dotenv()
 hf_token = os.getenv("HF_TOKEN")
+headers = {
+    "Authorization": f"Bearer {hf_token}",
+}
 
-model_id = "sentence-transformers/all-MiniLM-L6-v2"
-api_url = f"https://api-inference.huggingface.co/pipeline/feature-extraction/{model_id}"
-headers = {"Authorization": f"Bearer {hf_token}"}
-
-def generate_embeddings(texts):
-    response = requests.post(api_url, headers=headers, json={"inputs": texts, "options":{"wait_for_model":True}})
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
     return response.json()
+
+output = query({
+    "inputs": "Today is a sunny day and I will get some ice cream.",
+})
+def generate_embeddings(texts):
+    embeddings = query({
+        "inputs": texts,})
+    return embeddings
